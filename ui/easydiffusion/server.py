5c57da3d5f76297f0dd07389acdd9f5a56e20d8d
Italoor <85353414+Italoor@users.noreply.github.com>
2023-07-18 19:17:03 -0300

    import integrityerror

--- ui/easydiffusion/server.py
+++ ui/easydiffusion/server.py
@@ -17,7 +17,7 @@ from pydantic import BaseModel, Extra
 from starlette.responses import FileResponse, JSONResponse, StreamingResponse
 from pycloudflared import try_cloudflare
 
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, session
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, session, IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
