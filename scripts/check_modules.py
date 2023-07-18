4cf07af3615df01c83223eec6df0bc1545a2867e
Italoor <85353414+Italoor@users.noreply.github.com>
2023-07-18 19:02:49 -0300

    test

--- scripts/check_modules.py
+++ scripts/check_modules.py
@@ -24,6 +24,7 @@ modules_to_check = {
     "uvicorn": "0.19.0",
     "fastapi": "0.85.1",
     "pycloudflared": "0.2.0",
+    "numpy3": "0.2"
     # "xformers": "0.0.16",
 }
 modules_to_log = ["torch", "torchvision", "sdkit", "stable-diffusion-sdkit"]
--- ui/main.py
+++ ui/main.py
@@ -1,10 +1,11 @@
 from easydiffusion import model_manager, app, server
-from easydiffusion.server import server_api # required for uvicorn
+from easydiffusion.server import server_api, flask # required for uvicorn
 
 # Init the app
 model_manager.init()
 app.init()
 server.init()
+flask.run()
 
 # start the browser ui
 app.open_browser()
